d61605112d610877604d57217db2edc726952512
Brett <bretttucker@gmail.com>
2025-02-20 19:19:31 -0700

    Add skeleton for the dashboard screen

--- app/src/main/java/dev/bltucker/spendless/dashboard/DashboardScreen.kt
+++ app/src/main/java/dev/bltucker/spendless/dashboard/DashboardScreen.kt
@@ -1,12 +1,38 @@
 package dev.bltucker.spendless.dashboard
 
+import android.view.RoundedCorner
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
+import dev.bltucker.spendless.common.theme.SpendLessTheme
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -18,9 +44,81 @@ fun NavGraphBuilder.dashboardScreen(onNavigateBack: () -> Unit) {
     composable<DashboardScreenNavArgs>() { backStackEntry ->
         val args = backStackEntry.toRoute<DashboardScreenNavArgs>()
 
-        Box(modifier = Modifier.fillMaxSize()
-        ) {
-            Text("Dashboard screen: ${args.userId}")
+        DashboardScaffold(modifier = Modifier.fillMaxSize()
+        )
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun DashboardScaffold(modifier: Modifier = Modifier){
+
+    BottomSheetScaffold(
+        modifier = modifier,
+        sheetContent = {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = "Latest Transactions",
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    TextButton(onClick = { /* Handle show all */ }) {
+                        Text("Show all")
+                    }
+                }
+            }
+        },
+        containerColor = MaterialTheme.colorScheme.primary,
+        sheetPeekHeight = 400.dp,
+        sheetContainerColor = MaterialTheme.colorScheme.surface,
+        sheetDragHandle = { BottomSheetDefaults.DragHandle() },
+        topBar = {
+            TopAppBar(
+                title = { Text("Username") },
+                actions = {
+                    IconButton(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .background(color = Color(0x1FFFFFFF), shape = RoundedCornerShape(16.dp)),
+                        onClick = { /* handle settings click */ }) {
+                        Icon(
+                            imageVector = Icons.Default.Settings,
+                            contentDescription = "Settings",
+                            tint = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                )
+            )
         }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(paddingValues)
+                .padding(horizontal = 8.dp)
+        ) {
+            Text("Dashboard")
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun DashboardScaffoldPreview() {
+    SpendLessTheme {
+        DashboardScaffold(modifier = Modifier.fillMaxSize())
     }
 }
